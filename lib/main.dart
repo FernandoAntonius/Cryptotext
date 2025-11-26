2c7d317d8895c3be853f6a346212de39bc74dc23
FernandoAntonius <fernandoanthonius_2428250014@mhs.mdp.ac.id>
2025-11-26 10:52:20 +0700

    shared preference

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,69 @@
+// Import paket-paket yang dibutuhkan
 import 'package:flutter/material.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:logger/logger.dart';
 
+// Fungsi main yang menjalankan aplikasi
 void main() {
-  runApp(const CryptoText());
+  runApp(const MyApp());
 }
 
-class CryptoText extends StatelessWidget {
-  const CryptoText({super.key});
-
+// Kelas MyApp, merupakan tampilan awal aplikasi
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CryptoText',
-      home: HomeScreen(),
+      home:
+          SignInScreen(), // Halaman pertama yang ditampilkan adalah SignInPage,
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreen();
-}
-
-class _HomeScreen extends State<HomeScreen> {
-  final TextEditingController _textEditingController = TextEditingController();
-
-  // Kunci untuk enkripsi dan deskripsi
-  final encrypt.Key _key = encrypt.Key.fromLength(32);
-
-  // Initialization vector untuk enkripsi dan deskripsi
-  final iv = encrypt.IV.fromLength(16);
-
-  String _encryptedText = '';
-  String _decryptedText = '';
-  String? _errorText;
-  bool _isDecryptedButtonEnabled = false;
-
+// Kelas SignUpScreen, tampilan untuk proses pendaftaran
+class SignUpScreen extends StatelessWidget {
+  // Controllers untuk input username dan password
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final Logger _logger = Logger(); // Untuk logging
+  SignUpScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('CryptoText')),
+      appBar: AppBar(
+        title: const Text('Sign Up'), // Judul halaman
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+          children: <Widget>[
+            // Input field untuk username
             TextField(
-              controller: _textEditingController,
-              decoration: InputDecoration(
-                labelText: 'Input text',
-                errorText: _errorText,
-                border: const OutlineInputBorder(),
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
               ),
-              onChanged: _onTextChanged,
+            ),
+            const SizedBox(height: 20),
+            // Input field untuk password
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                String inputText = _textEditingController.text;
-                if (inputText.isNotEmpty) {
-                  _encryptText(inputText);
-                } else {
-                  setState(() {
-                    _errorText = 'Input cannot be empty';
-                  });
-                }
+                _performSignUp(context); // Tombol Sign Up
               },
-              child: const Text('Encrypt'),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Encrypted text: $_encryptedText',
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _isDecryptedButtonEnabled && _encryptedText.isNotEmpty
-                  ? () {
-                      String inputText = _encryptedText;
-                      final encrypted = encrypt.Encrypted.fromBase64(inputText);
-                      _decryptText(encrypted.base64);
-                    }
-                  : null,
-              child: const Text('Decrypt'),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Decrypted text: $_decryptedText',
-              style: const TextStyle(fontWeight: FontWeight.bold),
+              child: const Text('Sign Up'),
             ),
           ],
         ),
@@ -98,44 +71,325 @@ class _HomeScreen extends State<HomeScreen> {
     );
   }
 
-  void _onTextChanged(String text) {
-    setState(() {
-      _isDecryptedButtonEnabled = text.isNotEmpty;
-      _encryptedText = '';
-      _decryptedText = '';
-      _errorText = null;
-    });
-  }
-
-  void _encryptText(String text) {
+  // Fungsi untuk melakukan proses sign up
+  void _performSignUp(BuildContext context) {
     try {
-      if (text.isNotEmpty) {
-        final encrypter = encrypt.Encrypter(encrypt.AES(_key));
-        final encrypted = encrypter.encrypt(text, iv: iv);
-        setState(() {
-          _encryptedText = encrypted.base64;
+      final prefs = SharedPreferences.getInstance();
+      _logger.d('Sign up attempt');
+      final String username = _usernameController.text;
+      final String password = _passwordController.text;
+      // Memeriksa apakah username atau password kosong sebelum melanjutkan sign-up
+      if (username.isNotEmpty && password.isNotEmpty) {
+        final encrypt.Key key = encrypt.Key.fromLength(32);
+        final iv = encrypt.IV.fromLength(16);
+        final encrypter = encrypt.Encrypter(encrypt.AES(key));
+        final encryptedUsername = encrypter.encrypt(username, iv: iv);
+        final encryptedPassword = encrypter.encrypt(password, iv: iv);
+        _saveEncryptedDataToPrefs(
+          prefs,
+          encryptedUsername.base64,
+          encryptedPassword.base64,
+          key.base64,
+          iv.base64,
+        ).then((_) {
+          Navigator.pop(context);
+          _logger.d('Sign up succeeded');
         });
       } else {
-        print('Text to encrypt cannot be empty');
+        _logger.e('Username or password cannot be empty');
       }
-    } catch (e, stackTrace) {
-      print('Error encrypting text: $e, stackStrace: $stackTrace');
+    } catch (e) {
+      _logger.e('An error occurred: $e');
     }
   }
 
-  void _decryptText(String text) {
-    try {
-      if (text.isNotEmpty) {
-        final encrypter = encrypt.Encrypter(encrypt.AES(_key));
-        final decrypted = encrypter.decrypt64(text, iv: iv);
-        setState(() {
-          _decryptedText = decrypted;
-        });
-      } else {
-        print('Text to encrypt cannot be empty');
-      }
-    } catch (e, stackTrace) {
-      print('Error encrypting text: $e, stackStrace: $stackTrace');
-    }
+  // Fungsi untuk menyimpan data terenkripsi ke SharedPreferences
+  Future<void> _saveEncryptedDataToPrefs(
+    Future<SharedPreferences> prefs,
+    String encryptedUsername,
+    String encryptedPassword,
+    String keyString,
+    String ivString,
+  ) async {
+    final sharedPreferences = await prefs;
+    // Logging: menyimpan data pengguna ke SharedPreferences
+    _logger.d('Saving user data to SharedPreferences');
+    await sharedPreferences.setString('username', encryptedUsername);
+    await sharedPreferences.setString('password', encryptedPassword);
+    await sharedPreferences.setString('key', keyString);
+    await sharedPreferences.setString('iv', ivString);
   }
 }
+
+// Kelas SignInScreen, tampilan untuk proses sign in
+class SignInScreen extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final Logger _logger = Logger(); // Untuk logging
+  SignInScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'), // Judul halaman
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // Input field untuk username
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Input field untuk password
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            // Tombol Sign In
+            ElevatedButton(
+              onPressed: () {
+                _performSignIn(context);
+              },
+              child: const Text('Sign In'),
+            ),
+            const SizedBox(height: 20),
+            // Tombol untuk pindah ke halaman pendaftaran
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpScreen()),
+                );
+              },
+              child: const Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Fungsi untuk melakukan proses sign in
+  void _performSignIn(BuildContext context) {
+    try {
+      final prefs = SharedPreferences.getInstance();
+      final String username = _usernameController.text;
+      final String password = _passwordController.text;
+      _logger.d('Sign in attempt');
+      if (username.isNotEmpty && password.isNotEmpty) {
+        _retrieveAndDecryptDataFromPrefs(prefs).then((data) {
+          if (data.isNotEmpty) {
+            final decryptedUsername = data['username'];
+            final decryptedPassword = data['password'];
+            if (username == decryptedUsername &&
+                password == decryptedPassword) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const HomeScreen()),
+              );
+              _logger.d('Sign in succeeded');
+            } else {
+              _logger.e('Username or password is incorrect');
+            }
+          } else {
+            _logger.e('No stored credentials found');
+          }
+        });
+      } else {
+        _logger.e('Username and password cannot be empty');
+        // Tambahkan pesan untuk kasus ketika username atau password kosong
+      }
+    } catch (e) {
+      _logger.e('An error occurred: $e');
+    }
+  }
+
+  // Fungsi untuk mengambil dan mendekripsi data dari SharedPreferences
+  Future<Map<String, String>> _retrieveAndDecryptDataFromPrefs(
+    Future<SharedPreferences> prefs,
+  ) async {
+    final sharedPreferences = await prefs;
+    final encryptedUsername = sharedPreferences.getString('username') ?? '';
+    final encryptedPassword = sharedPreferences.getString('password') ?? '';
+    final keyString = sharedPreferences.getString('key') ?? '';
+    final ivString = sharedPreferences.getString('iv') ?? '';
+    final encrypt.Key key = encrypt.Key.fromBase64(keyString);
+    final iv = encrypt.IV.fromBase64(ivString);
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    final decryptedUsername = encrypter.decrypt64(encryptedUsername, iv: iv);
+    final decryptedPassword = encrypter.decrypt64(encryptedPassword, iv: iv);
+    // Mengembalikan data terdekripsi
+    return {'username': decryptedUsername, 'password': decryptedPassword};
+  }
+}
+
+// Kelas HomeScreen, halaman utama setelah berhasil sign in
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'), // Judul halaman
+      ),
+      body: const Center(
+        child: Text('Welcome!'), // Pesan selamat datang
+      ),
+    );
+  }
+}
+
+// KRIPTOGRAFI
+// import 'package:flutter/material.dart';
+// import 'package:encrypt/encrypt.dart' as encrypt;
+
+// void main() {
+//   runApp(const CryptoText());
+// }
+
+// class CryptoText extends StatelessWidget {
+//   const CryptoText({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'CryptoText',
+//       home: HomeScreen(),
+//     );
+//   }
+// }
+
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   State<HomeScreen> createState() => _HomeScreen();
+// }
+
+// class _HomeScreen extends State<HomeScreen> {
+//   final TextEditingController _textEditingController = TextEditingController();
+
+//   // Kunci untuk enkripsi dan deskripsi
+//   final encrypt.Key _key = encrypt.Key.fromLength(32);
+
+//   // Initialization vector untuk enkripsi dan deskripsi
+//   final iv = encrypt.IV.fromLength(16);
+
+//   String _encryptedText = '';
+//   String _decryptedText = '';
+//   String? _errorText;
+//   bool _isDecryptedButtonEnabled = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('CryptoText')),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: [
+//             TextField(
+//               controller: _textEditingController,
+//               decoration: InputDecoration(
+//                 labelText: 'Input text',
+//                 errorText: _errorText,
+//                 border: const OutlineInputBorder(),
+//               ),
+//               onChanged: _onTextChanged,
+//             ),
+//             const SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: () {
+//                 String inputText = _textEditingController.text;
+//                 if (inputText.isNotEmpty) {
+//                   _encryptText(inputText);
+//                 } else {
+//                   setState(() {
+//                     _errorText = 'Input cannot be empty';
+//                   });
+//                 }
+//               },
+//               child: const Text('Encrypt'),
+//             ),
+//             const SizedBox(height: 10),
+//             Text(
+//               'Encrypted text: $_encryptedText',
+//               style: const TextStyle(fontWeight: FontWeight.bold),
+//             ),
+//             const SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: _isDecryptedButtonEnabled && _encryptedText.isNotEmpty
+//                   ? () {
+//                       String inputText = _encryptedText;
+//                       final encrypted = encrypt.Encrypted.fromBase64(inputText);
+//                       _decryptText(encrypted.base64);
+//                     }
+//                   : null,
+//               child: const Text('Decrypt'),
+//             ),
+//             const SizedBox(height: 10),
+//             Text(
+//               'Decrypted text: $_decryptedText',
+//               style: const TextStyle(fontWeight: FontWeight.bold),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   void _onTextChanged(String text) {
+//     setState(() {
+//       _isDecryptedButtonEnabled = text.isNotEmpty;
+//       _encryptedText = '';
+//       _decryptedText = '';
+//       _errorText = null;
+//     });
+//   }
+
+//   void _encryptText(String text) {
+//     try {
+//       if (text.isNotEmpty) {
+//         final encrypter = encrypt.Encrypter(encrypt.AES(_key));
+//         final encrypted = encrypter.encrypt(text, iv: iv);
+//         setState(() {
+//           _encryptedText = encrypted.base64;
+//         });
+//       } else {
+//         print('Text to encrypt cannot be empty');
+//       }
+//     } catch (e, stackTrace) {
+//       print('Error encrypting text: $e, stackStrace: $stackTrace');
+//     }
+//   }
+
+//   void _decryptText(String text) {
+//     try {
+//       if (text.isNotEmpty) {
+//         final encrypter = encrypt.Encrypter(encrypt.AES(_key));
+//         final decrypted = encrypter.decrypt64(text, iv: iv);
+//         setState(() {
+//           _decryptedText = decrypted;
+//         });
+//       } else {
+//         print('Text to encrypt cannot be empty');
+//       }
+//     } catch (e, stackTrace) {
+//       print('Error encrypting text: $e, stackStrace: $stackTrace');
+//     }
+//   }
+// }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,8 @@ import 'package:cryptotext/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const CryptoText());
+    await tester.pumpWidget(const MyApp());
+    // await tester.pumpWidget(const CryptoText());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
